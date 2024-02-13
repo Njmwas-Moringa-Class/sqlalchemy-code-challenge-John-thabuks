9a20bc5858073ac3a668a1ae9cd8a840648b0cbb
John-thabuks <thabuksjohn@gmail.com>
2024-02-14 00:05:16 +0300

    Review migration

---
+++ lib/migrations/versions/91c52f871500_reviews_migration.py
@@ -0,0 +1,24 @@
+"""reviews migration
+
+Revision ID: 91c52f871500
+Revises: 2723e7bbcf91
+Create Date: 2024-02-14 00:04:33.846932
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '91c52f871500'
+down_revision = '2723e7bbcf91'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/models.py
+++ lib/models.py
@@ -3,17 +3,28 @@ import sys
 
 sys.path.append(os.getcwd)
 
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer, ForeignKey, Table)
 
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 engine = create_engine('sqlite:///db/restaurants.db', echo=True)
 
+# Creates the association table 
+restaurants_customers = Table('restaurants_customers', Base.metadata,
+    Column('restaurant_id', Integer, ForeignKey('restaurants.id')),
+    Column('customer_id', Integer, ForeignKey('customers.id')))
 
-# class Review(Base):
-#     pass
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    star_rating = Column(Integer)
+
+    customer = relationship("Customer", back_populates="reviews")
+    restaurant = relationship("Restaurant", back_populates="reviews")
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -21,6 +32,9 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String())
     price = Column(Integer)
+
+    reviews = relationship("Review", back_populates="restaurant")
+    customers = relationship("Customer", secondary=restaurants_customers, back_populates="restaurants")
 
     def __repr__(self):
         return f'Restaurant: {self.name}'
@@ -32,5 +46,10 @@ class Customer(Base):
     first_name = Column(String())
     last_name = Column(String())
 
+    reviews = relationship("Review", back_populates="customer")
+    restaurants = relationship("Restaurant", secondary=restaurants_customers, back_populates="customers")
+
     def __repr__(self):
         return f'Customer: {self.name}'
+
+
